1b647e8ed283b3fdb44e22dcb1d26fa2099f8a5c
Geekloop <soto.r.abigail@gmail.com>
2018-05-26 12:54:07 -0400

    primer commit

---
+++ ejemplo.py
@@ -0,0 +1,11 @@
+#! C:\Python27
+# -*- coding: iso-8859-15 -*-
+import os, sys
+print ("Ingrese numeros a sumar:")
+n1 = int(input(""))
+n2 = int(input(""))
+n3 = int(input(""))
+n4 = int(input(""))
+suma = (n1 + n2 + n3 + n4)
+cadena = "El resultado es"
+print (cadena, suma)
